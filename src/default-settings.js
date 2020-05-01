f727306bd51e57a85fd5154b7478835172fa0b06
thefantas <thefantas@gmail.com>
2020-05-01 15:52:29 +0300

    Update default-settings.js

--- src/default-settings.js
+++ src/default-settings.js
@@ -3,13 +3,15 @@ const kDefaultSettings = {
   lowerBaselinePos: 0.85,
   primaryImageScale: 0.75,
   primaryImageOpacity: 0.85,
-  primaryTextScale: 0.95,
+  primaryTextScale: 1.2,
   primaryTextOpacity: 0.85,
-  secondaryImageScale: 0.5,
+  secondaryImageScale: 0.75,
   secondaryImageOpacity: 0.85,
-  secondaryTextScale: 1.0,
+  secondaryTextScale: 1.2,
   secondaryTextStroke: 2.0,
   secondaryTextOpacity: 0.85,
+  rewind: 3000,
+  fastForward: 3000,
 };
 
 module.exports = kDefaultSettings;
